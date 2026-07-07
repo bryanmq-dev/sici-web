c308837cf78988c9168d6cf68ff15cc444cab077
bryanmq-dev <mamani.bryan2505@gmail.com>
2026-07-07 01:56:00 -0400

    feat: added strapi cms project

---
+++ cms/src/api/posts-epc/controllers/posts-epc.ts
@@ -0,0 +1,7 @@
+/**
+ * posts-epc controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::posts-epc.posts-epc');
---
+++ cms/src/api/posts-epc/routes/posts-epc.ts
@@ -0,0 +1,7 @@
+/**
+ * posts-epc router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::posts-epc.posts-epc');
